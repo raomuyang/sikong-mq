38bc0edf13ec5ee82a6b9bc12ac02e37397d9a93
rao-mengnan <raomengnan@gmail.com>
2017-12-20 15:40:50 +0800

    remove local lock

--- skmq/cache.go
+++ skmq/cache.go
@@ -6,16 +6,12 @@ import (
 	"strconv"
 	"fmt"
 	"strings"
-	"sync"
 )
 
 var (
 	Pool      *redis.Pool
-	RdsLocker *RedisLocker
 )
 
-type RedisLocker struct {
-}
 
 func init() {
 	Pool = &redis.Pool{
@@ -445,43 +441,4 @@ func AddApplication(appId string) error  {
 func GetApps() []string {
 	list, _ := redis.Strings(Pool.Get().Do("SMEMBERS", KAppSet))
 	return list
-}
-
-/**
-	非阻塞
- */
-func (*RedisLocker) Lock(key string) (bool, error) {
-	var rwMutex sync.RWMutex
-
-	rwMutex.RLock()
-	dbConn := Pool.Get()
-	res, err := redis.Bool(dbConn.Do("SISMEMBER",  KStatusLockSet, key))
-	rwMutex.RUnlock()
-	if err != nil {
-		return false, UnknownDBOperationException{Detail: "get lock status failed, " + err.Error()}
-	}
-
-	if res {
-		return false, nil
-	}
-	rwMutex.Lock()
-	_, err = dbConn.Do("SADD", KStatusLockSet, key)
-	rwMutex.Unlock()
-	if err != nil {
-		return false, UnknownDBOperationException{Detail: "lock failed, " + err.Error()}
-	}
-	return true, nil
-}
-
-func (*RedisLocker) Unlock(key string) error {
-	dbConn := Pool.Get()
-	var mutex sync.Mutex
-	mutex.Lock()
-	defer mutex.Unlock()
-	_, err := dbConn.Do("SREM",  KStatusLockSet, key)
-	if err != nil {
-		return UnknownDBOperationException{Detail: "Unlock " + err.Error()}
-	}
-
-	return nil
 }
