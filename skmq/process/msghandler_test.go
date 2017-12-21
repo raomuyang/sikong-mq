57b2b6473ec9a37ad33399506c78c4012f4a6aee
rao-mengnan <raomengnan@gmail.com>
2017-12-21 18:14:00 +0800

    message handler

---
+++ skmq/process/msghandler_test.go
@@ -0,0 +1,70 @@
+package process
+
+import (
+	"fmt"
+	"testing"
+	"bytes"
+	"strings"
+	"github.com/sikong-mq/skmq/base"
+)
+
+var (
+	content = []byte("This is content")
+	testMsg = base.Message{
+		MsgId: "Test-msg-id",
+		AppID: "test-app-id",
+		Type: base.TopicMsg,
+		Content: content}
+)
+
+func testInvokeDecodeMessage() []base.Message {
+	input := make(chan []byte, 4)
+	go func() {
+
+		buf := append(EncodeMessage(testMsg), EncodeMessage(testMsg)...)
+		position := 0
+		for {
+			begin := position
+			end := position + 10
+			if end > len(buf) {
+				end = len(buf)
+			}
+			input <- buf[begin:end]
+			position += 10
+			if position >= len(buf) {
+				break
+			}
+		}
+		close(input)
+	}()
+
+	msgChan := DecodeMessage(input)
+	var list []base.Message
+	for {
+		msg, ok:= <-msgChan
+		if !ok {
+			break
+		}
+		list = append(list, msg)
+	}
+	return list
+}
+
+func TestDecodeMessage(t *testing.T) {
+	list := testInvokeDecodeMessage()
+	if len(list) != 2 {
+		t.Error("Decode failed.")
+	}
+	fmt.Printf("%v \n", list[0])
+	if !bytes.Equal([]byte(content), list[1].Content) ||
+		!(strings.Compare(list[0].MsgId, list[1].MsgId) == 0) {
+		t.Error("Message not equal.")
+	}
+}
+
+func BenchmarkDecodeMessage(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		testInvokeDecodeMessage()
+	}
+
+}
