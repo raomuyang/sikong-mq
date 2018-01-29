ec7c410f132c78e354d1cdb0e624c2e164bcb220
rao-mengnan <raomengnan@gmail.com>
2018-01-29 15:35:51 +0800

    使用退避的方式检索消息

--- skmq/base/util.go
+++ skmq/base/util.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"bufio"
 	"io"
+	"time"
+	"math/rand"
+	"math"
 )
 
 func UnmarshalJsonFile(instance interface{}, path string) error {
@@ -34,3 +37,36 @@ func UnmarshalJsonFile(instance interface{}, path string) error {
 	return nil
 }
 
+func RandSeconds(max int) time.Duration {
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+	return time.Duration(r.Intn(max))
+}
+
+type Backoff struct {
+	times int
+	Limit int
+}
+
+func (backoff *Backoff) Increase() time.Duration {
+	if backoff.times + 2 >= backoff.GetLimit() {
+		backoff.times = backoff.GetLimit()
+	} else {
+		backoff.times += 2
+	}
+
+	e := math.Log(float64(backoff.times))
+	t := e * float64(time.Minute)
+	return time.Duration(t) + RandSeconds(30)
+}
+
+func (backoff *Backoff) GetLimit() int {
+	if backoff.Limit == 0 {
+		return 30
+	}
+	return backoff.Limit
+}
+
+func (backoff *Backoff) Reset() {
+	backoff.times = 0
+}
