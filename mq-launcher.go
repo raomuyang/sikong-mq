46c7e1109d42b2785daaa1bfd2cf03a2217b3de3
rao-mengnan <raomengnan@gmail.com>
2018-02-06 18:27:52 +0800

    加载配置文件中的日志级别设置

--- mq-launcher.go
+++ mq-launcher.go
@@ -4,14 +4,24 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"encoding/json"
+	"net/http"
+	_ "net/http/pprof"
 	"github.com/sikong-mq/skmq"
 	"github.com/sikong-mq/skmq/base"
-	"encoding/json"
 	"github.com/sikong-mq/skmq/process"
 )
 
+const DebugPort  = 7185
+
 func main() {
 	LoadConf()
+	go func() {
+		http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", DebugPort), nil)
+	}()
+
+	process.SetLogOutWriter(nil)
+	process.SetLogLevel(process.Configuration.LogLevel)
 	skmq.OpenServer()
 }
 
